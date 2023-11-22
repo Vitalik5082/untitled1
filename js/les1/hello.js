22852c1e3784ac017e79fe97834ed1d3b01834f5
Vitalik5082 <avkvitala@gmail.com>
2023-11-22 21:43:02 +0200

    all first commit

---
+++ js/les1/hello.js
@@ -0,0 +1,35 @@
+//- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let hello='hello'
+let owu = 'owu'
+let com = 'com'
+let ua = 'ua'
+let one = 1
+let ten = 10
+let nine = -999
+let otf = 123
+let pi = 3.14
+let two = 2.7
+let oneS = 16
+let zero = false
+let oneTr= true
+
+let name = 'Vitaliy '
+let surname='Ratushnuak '
+let father= 'Oleksandrovich'
+console.log(name+surname+father)
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
